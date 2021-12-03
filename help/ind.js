7c991520bbf3cfdf8db664f89ad61d57f81463f3
Rtxc7 <fanzzxxx@gmail.com>
2021-12-03 16:11:19 +0700

    123

---
+++ help/ind.js
@@ -0,0 +1,255 @@
+exports.wait = () => {
+    return `⏳ Mohon tunggu sebentar~`
+}
+
+exports.ok = () => {
+    return `Ok desu~`
+}
+
+exports.err = () => {
+    return `‼️Fitur Sedang Error‼️
+
+⏳Sedang melapor bug fitur ke owner-sama⏳`
+}
+
+exports.wrongFormat = (prefix) => {
+    return `Format salah ‼️ Silakan cek cara penggunaan di *${prefix}allmenu*.`
+}
+
+exports.emptyMess = () => {
+    return `⚠️ Harap masukkan pesan yang ingin disampaikan! ⚠️`
+}
+
+exports.cmdNotFound = (cmd, prefix) => {
+    return `❎ Command *${cmd}* tidak terdaftar di *${prefix}allmenu* ❎`
+}
+
+exports.ownerOnly = () => {
+    return `⚠️ Command ini khusus Owner-sama! ⚠️`
+}
+
+exports.doneOwner = () => {
+    return `✔  ️Sudah selesai, Owner-sama~`
+}
+
+exports.groupOnly = () => {
+    return `👥  Command ini hanya bisa digunakan di dalam grup!`
+}
+
+exports.adminOnly = () => {
+    return `🙅  Command ini hanya bisa digunakan oleh admin grup!`
+}
+
+exports.nhFalse = () => {
+    return `Kode tidak valid!`
+}
+
+exports.listBlock = (blockNumber) => {
+    return `*── 「 HALL OF SHAME 」 ──*
+    
+Total diblokir: *${blockNumber.length}* user\n`
+}
+
+exports.notAdmin = () => {
+    return `❎ User bukan seorang admin! ❎`
+}
+
+exports.adminAlready = () => {
+    return `⚠️ Tidak  dapat mem-promote user yang merupakan admin! ⚠️`
+}
+
+exports.botNotAdmin = () => {
+    return `Jadikan bot sebagai admin terlebih dahulu! 🙏`
+}
+
+exports.received = (pushname) => {
+    return `
+Halo ${pushname}!
+Terima kasih telah melapor, laporanmu akan kami segera terima.`
+}
+
+exports.videoLimit = () => {
+    return `Ukuran file terlalu besar!`
+}
+
+exports.notNum = (q) => {
+    return `"${q}", bukan angka!`
+}
+
+exports.listMenu = (time, salam, pushname, prefix) => {
+    return `
+*Selamat ${salam} ${pushname}*
+*⌚Time Server : ${time}*
+
+
+*MY LIST MENU*
+
+
+🤖 *INFO*
+
+-> ${prefix}owner
+-> ${prefix}rules
+-> ${prefix}donate
+
+
+🧑 *OWNER*
+
+> 
+$ 
+-> ${prefix}join link
+-> ${prefix}setppbot (tag/send image)
+
+
+⚙️ *GROUP*
+
+-> ${prefix}revoke
+-> ${prefix}leave
+-> ${prefix}group open/close
+-> ${prefix}tagall text
+-> ${prefix}hidetag text
+
+
+😷 *WEEBS*
+
+-> ${prefix}anime query
+-> ${prefix}manga query
+-> ${prefix}character query
+-> ${prefix}quotesanime 
+-> ${prefix}hentai
+
+
+🔍 *MISC*
+
+-> ${prefix}film query
+-> ${prefix}wattpad query
+-> ${prefix}webtoons query
+-> ${prefix}drakor query
+-> ${prefix}pinterest query
+
+
+🎞 *MEDIA*
+
+-> ${prefix}toimg (tag sticker)
+
+
+⬇️ *DOWNLOADER* 
+
+-> ${prefix}tiktok link
+-> ${prefix}ytmp3 link
+-> ${prefix}ytmp4 link
+-> ${prefix}ytdl link
+-> ${prefix}facebook link
+-> ${prefix}twitter link
+-> ${prefix}instagram link
+
+
+📫 *ANIME RANDOM* 
+
+-> ${prefix}loli
+-> ${prefix}neko
+-> ${prefix}waifu
+-> ${prefix}shinobu
+-> ${prefix}megumin
+-> ${prefix}bully
+-> ${prefix}cuddle
+-> ${prefix}cry
+-> ${prefix}hug
+-> ${prefix}awoo
+-> ${prefix}kiss
+-> ${prefix}lick
+-> ${prefix}pat
+-> ${prefix}smug
+-> ${prefix}bonk
+-> ${prefix}yeet
+-> ${prefix}blush
+-> ${prefix}smile
+-> ${prefix}wave
+-> ${prefix}highfive
+-> ${prefix}handhold
+-> ${prefix}nom
+-> ${prefix}bite
+-> ${prefix}glomp
+-> ${prefix}slap
+-> ${prefix}kill
+-> ${prefix}happy
+-> ${prefix}wink
+-> ${prefix}poke
+-> ${prefix}dance
+-> ${prefix}cringe
+
+
+BASE MD BY
+
+- Rasyid💙
+
+TQ FOR
+
+- Cahyo🕊
+- Dimas🕊
+- Rio🕊
+    `
+}
+
+exports.rules = (prefix) => {
+    return `
+*── 「 RULES AND FAQ 」 ──*
+
+1. Jangan spam bot. 🙅
+Sanksi: *⚠️ WARN/SOFT BLOCK*
+
+2. Jangan telepon bot. ☎️
+Sanksi: *❎ SOFT BLOCK*
+
+3. Jangan mengeksploitasi bot.😖
+Sanksi: *‼️ PERMANENT BLOCK ‼️*
+
+🗯️ Bot tidak atau lambat merespon ?
+➡️ Mungkin dipengaruhi oleh jaringan, signal, banned oleh Whatsapp dan beberapa asalan. Tetap patuhi rules‼️
+
+🗯️ Dimana saya bisa mendapatkan Script dari bot ini ?
+➡️ Script ini masih private dan tidak pernah diperjual belikan ,bijaklah dalam mengetahui penipu.
+
+🗯️ Boleh saya menambah ke grup?
+➡️ Untuk sementara bot dalam status free to add.
+
+🗯️ Prefixnya apa ya?
+➡️ Bot ini menggunakan multi prefix. Berarti anda bisa menggunakan prefix #, . , Dan prefix wajar lainnya.
+
+🗯️ Kak, kok syaa chat owner tidak direspon?
+➡️ Owner hanya merespon pertanyaan seputar bot Dan kendala eror, tidak untuk kenalan ataupun mengemis script.
+
+
+Jika sudah dipahami rules-nya, silakan ketik *${prefix}allmenu* untuk memulai!
+
+⚠️ Segala kebijakan dan ketentuan Alphabot di pegang oleh owner dan segala perubahan kebijakan, sewaktu waktu owner berhak mencabut, memblokir user(*﹏*) 
+
+Arigatou Gozaimasu! Untuk kalian user ramah dan Beberapa orang yg ikut membantu juga dalam project pembuatan Alphabot
+😖🙏
+
+
+    `
+}
+exports.source = (prefix) =>{
+return`*── 「 SOURCE 」 ──*
+
+Original : https://github.com/rashidsiregar28/chikabot
+Sc Recode : https://github.com/zeeoneofc/Alphabot-Md
+`
+}
+exports.tos = (ownerNumber, prefix) => {
+    return `
+*── 「 DONATE 」 ──*
+
+Hai 👋
+Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
+🏧 0887435047326 (OVO/Dana/GoPay)
+
+Berapapun donasi kalian akan sangat berarti 👍
+
+Arigatou!
+
+Contact person Owner:
+wa.me/${ownerNumber} (Owner)
+
+    `
+}
---
+++ noapikey/scraper.js
@@ -0,0 +1,58 @@
+/**
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+
+function hentai() {
+    return new Promise((resolve, reject) => {
+        const page = Math.floor(Math.random() * 1153)
+        axios.get('https://sfmcompile.club/page/'+page)
+        .then((data) => {
+            const $ = cheerio.load(data.data)
+            const hasil = []
+            $('#primary > div > div > ul > li > article').each(function (a, b) {
+                hasil.push({
+                    title: $(b).find('header > h2').text(),
+                    link: $(b).find('header > h2 > a').attr('href'),
+                    category: $(b).find('header > div.entry-before-title > span > span').text().replace('in ', ''),
+                    share_count: $(b).find('header > div.entry-after-title > p > span.entry-shares').text(),
+                    views_count: $(b).find('header > div.entry-after-title > p > span.entry-views').text(),
+                    type: $(b).find('source').attr('type') || 'image/jpeg',
+                    video_1: $(b).find('source').attr('src') || $(b).find('img').attr('data-src'),
+                    video_2: $(b).find('video > a').attr('href') || ''
+                })
+            })
+            resolve(hasil)
+        })
+    })
+}
+
+function quotesAnime() {
+    return new Promise((resolve, reject) => {
+        const page = Math.floor(Math.random() * 184)
+        axios.get('https://otakotaku.com/quote/feed/'+page)
+        .then(({ data }) => {
+            const $ = cheerio.load(data)
+            const hasil = []
+            $('div.kotodama-list').each(function(l, h) {
+                hasil.push({
+                    link: $(h).find('a').attr('href'),
+                    gambar: $(h).find('img').attr('data-src'),
+                    karakter: $(h).find('div.char-name').text().trim(),
+                    anime: $(h).find('div.anime-title').text().trim(),
+                    episode: $(h).find('div.meta').text(),
+                    up_at: $(h).find('small.meta').text(),
+                    quotes: $(h).find('div.quote').text().trim()
+                })
+            })
+            resolve(hasil)
+        }).catch(reject)
+    })
+}
+
+module.exports = { hentai, quotesAnime }
